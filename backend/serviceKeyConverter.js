870d784686df078f5de558227b932f30bc0879c3
Rifat240510 <rifat240510@gmail.com>
2025-12-03 23:56:15 +0600

    b12-m11-conceptual-session part 01 fully complete

--- backend/serviceKeyConverter.js
+++ backend/serviceKeyConverter.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const jsonData = fs.readFileSync('./serviceAccountConverter.js.json')
+const jsonData = fs.readFileSync('./serviceAccountConverter.json')
 
 const base64String = Buffer.from(jsonData, 'utf-8').toString('base64')
 console.log(base64String)
