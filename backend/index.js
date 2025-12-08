caa12b1ba9b5bf1f4b78fb66620622c8db661cc5
Rifat240510 <rifat240510@gmail.com>
2025-12-08 23:05:41 +0600

    verifyJWT added for backend security

--- backend/index.js
+++ backend/index.js
@@ -171,10 +171,8 @@ async function run() {
 
 
      // get all orders for a customer by email
-    app.get('/my-orders/:email', async (req, res) => {
-      const email = req.params.email
-
-      const result = await ordersCollection.find({ customer: email }).toArray()
+    app.get('/my-orders', verifyJWT, async (req, res) => {
+      const result = await ordersCollection.find({ customer: req.tokenEmail}).toArray()
       res.send(result)
     })
 
@@ -236,15 +234,11 @@ async function run() {
 
 
     // get a user's role
-    // app.get('/user/role', verifyJWT, async (req, res) => {
-      //   const result = await usersCollection.findOne({ email: req.tokenEmail })
-      //   res.send({ role: result?.role })
-      // })
-app.get('/user/role/:email', async (req, res) => {
-  const email = req.params.email
-  const result = await usersCollection.findOne({ email })
-  res.send({ role: result?.role })
-})
+    app.get('/user/role', verifyJWT, async (req, res) => {
+        const result = await usersCollection.findOne({ email: req.tokenEmail })
+        res.send({ role: result?.role })
+      })
+
 
 
 
